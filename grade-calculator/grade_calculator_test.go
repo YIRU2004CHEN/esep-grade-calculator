6e583712cbfc5bf4f8c1ae9b18393ea3d876fbf6
Yiru Chen <chenyiru@ufl.edu>
2025-09-28 13:47:38 -0400

    commit 3: add extra tests to reach the 100% coverage for Part 4.

--- grade-calculator/grade_calculator_test.go
+++ grade-calculator/grade_calculator_test.go
@@ -49,3 +49,62 @@ func TestGetGradeF(t *testing.T) {
 		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
 	}
 }
+
+// added new test
+
+// make sure the GradeType.String() returns the correct string type
+func TestGradeTypeString(t *testing.T) {
+    if Assignment.String() != "assignment" {
+        t.Errorf("Expected 'assignment', got %s", Assignment.String())
+    }
+    if Exam.String() != "exam" {
+        t.Errorf("Expected 'exam', got %s", Exam.String())
+    }
+    if Essay.String() != "essay" {
+        t.Errorf("Expected 'essay', got %s", Essay.String())
+    }
+}
+
+
+
+func TestGetGradeC(t *testing.T) {
+	expected_value := "C"
+
+	gradeCalculator := NewGradeCalculator()
+
+	gradeCalculator.AddGrade("week 5 assignment", 70, Assignment)
+	gradeCalculator.AddGrade("exam 5", 70, Exam)
+	gradeCalculator.AddGrade("essay on anthropology", 73, Essay)
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+func TestGetGradeD(t *testing.T) {
+	expected_value := "D"
+
+	gradeCalculator := NewGradeCalculator()
+
+	gradeCalculator.AddGrade("open source assignment", 60, Assignment)
+	gradeCalculator.AddGrade("exam 1", 60, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 63, Essay)
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+// if theres no grade when calculating the grades, return 0 rather than error
+func TestComputeAverageEmptyList(t *testing.T) {
+    emptyGrades := []Grade{}
+    avg := computeAverage(emptyGrades)
+    if avg != 0 {
+        t.Errorf("Expected 0 for empty list, got %d", avg)
+    }
+}
+
